0d0395c640fb4c772a108d518e4c0fb50391a540
alex <yougooo10@gmail.com>
2016-06-07 11:56:18 +0300

    file

--- new.py
+++
@@ -1,8 +0,0 @@
-#commit
-def enter_name():
-    name = raw_input("Enter your name: ")
-    if name:
-        return name
-    else:
-        print "Sorry, try again, enter your name."
-        return enter_name()
