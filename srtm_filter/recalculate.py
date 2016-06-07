15b87cf6a990f44f1fc93d21287893c6470acaec
JaneKravchenko <janekravchenko09071996@gmail.com>
2016-06-07 08:41:42 +0300

    update calculate recalculate file

--- srtm_filter/recalculate.py
+++ srtm_filter/recalculate.py
@@ -1,3 +1,4 @@
+import numpy as np
 def recalculate_array(temp_array,posi, posj, std, mean):
     """
     :param temp_array: temporary moving window array
@@ -9,11 +10,23 @@ def recalculate_array(temp_array,posi, posj, std, mean):
     """
     width = len(temp_array)
     if width%2 == 1:
-        mean_posi = width/2+1
-        mean_posj = width/2+1
-        if temp_array[mean_posi][mean_posj] in range(mean-std, mean+std):
+        mean_posi = width/2
+        mean_posj = width/2
+        if temp_array[mean_posi][mean_posj] > mean-std and temp_array[mean_posi][mean_posj] < mean+std:
             return {(mean_posi+posi, mean_posj+posj): temp_array[mean_posi][mean_posj]}
         else:
-            return {(mean_posi+posi, mean_posj+posj): temp_array[mean_posi][mean_posj]}
+            normal_value = []
+            for i in temp_array:
+                for j in i:
+                    if j <= mean+(2*std) and j >= mean-(2*std):
+                        normal_value.append(j)
+            if len(normal_value) > 1:
+                mean = np.mean(normal_value)
+                print '+++ '+ str(mean)
+                return {(mean_posi+posi, mean_posj+posj): mean}
+            else:
+                print 'Not changed mean: '+ str(mean)
+                return {(mean_posi + posi, mean_posj + posj): mean}
     else:
         return str('Moving window width and height mus be unpaired')
+
