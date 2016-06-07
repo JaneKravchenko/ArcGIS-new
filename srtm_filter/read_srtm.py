0d8835d2abdb467272568e0d51efced04eedcf49
JaneKravchenko <janekravchenko09071996@gmail.com>
2016-06-07 08:42:05 +0300

    update read_srtm_file

--- srtm_filter/read_srtm.py
+++ srtm_filter/read_srtm.py
@@ -1,11 +1,14 @@
 import gdal
 import numpy as np
 
-def read_srtm(path):
+'''def read_srtm(path):
     """
     :param path: way to .tif file
     :return: numpy array dtype: float32
     """
     image = gdal.Open(path)
-    return image.ReadAsArray().astype(np.float32)
+    return image.ReadAsArray().astype(np.float32)'''
 
+
+
+read_srtm = lambda path: gdal.Open(path).ReadAsArray().astype(np.float32)
